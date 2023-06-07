9b68db4e1e3984d44375a91b2c1e89455c73f93f
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-06-06 21:43:54 -0500

    added code to most of the todos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,68 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('jate_db', 1, {
+    // switched the starter code from 'jate' to jate_db
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (db.objectStoreNames.contains('content')) {
         console.log('jate database already exists');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      // Do i switch all of the "content" to jate? 
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// copy and pasted (with a few tweaks from class code)
+
+export const putDb = async (content) => {
+  // console.log('Post to the ase');
+
+  // // Create a connection to the database database and version we want to use.
+  // const jate_db = await openDB('jate_db', 1);
+  // // In the example code they named the database jate? 
+
+  // // Create a new transaction and specify the database and data privileges.
+  // const tx = jate_db.transaction('content', 'readwrite');
+
+  // // Open up the desired object store.
+  // const store = tx.objectStore('content');
+
+  // // Use the .add() method on the store and pass in the content.
+  // const request = store.add({ todo: content });
+
+  // // Get confirmation of the request.
+  // const result = await request;
+  // console.log('🚀 - data saved to the database', result);
+
+  console.error('putDb not implemented');
+} 
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {  
+  console.error('getDb not implemented');
+//   console.log('GET all from the database');
+
+// // Create a connection to the database database and version we want to use.
+// const jate_db = await openDB('jate', 1);
+
+// // Create a new transaction and specify the database and data privileges.
+// const tx = jate_db.transaction('content', 'readonly');
+
+// // Open up the desired object store.
+// const store = tx.objectStore('content');
+
+// // Use the .getAll() method to get all data in the database.
+// const request = store.getAll();
+
+//  // Get confirmation of the request.
+// const result = await request;
+// console.log('result.value', result);
+// return result;
+} 
+
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,24 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // event.preventDefault();
+    // buttonInstall.style.visibility = 'visible';
+    // textHeader.textContent = 'Click the button to install!';
+    // added the textHeader to the HTML. Don't know if i needed to do that 
+
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    // event.prompt();
+    // buttonInstall.setAttribute('disabled', true);
+    // buttonInstall.textContent = 'Installed!'; 
+
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // textHeader.textContent = 'Successfully installed!';
+    // console.log('👍', 'appinstalled', event);
+});
